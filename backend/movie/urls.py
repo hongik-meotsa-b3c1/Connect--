49a4b704c0be84d50d9c1960a48fadfbab79092b
Park-Seonhyeong <Park-Seonhyeong>
2022-02-27 21:49:09 +0900

    movie post make

--- backend/movie/urls.py
+++ backend/movie/urls.py
@@ -6,7 +6,7 @@ app_name='movie'
 
 urlpatterns = [
     path('search/', views.MovieInfoAPIView.as_view(), name='api_search'),
-
+    path('postWrite/', views.PostWrite.as_view(), name='postWrite'),
     # url(r'(?P<pk>[0-9]+)/', views.MovieDetailAPIView.as_view(), name='detail'),
     
 
